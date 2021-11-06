01da85b4b6eabb59a52d9014ed7d4eefe528308f
w3bdev1 <w3bdev@protonmail.com>
2021-11-06 14:49:07 +0530

    Title added. Style added

--- index.js
+++ index.js
@@ -3,20 +3,18 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-let url;
-
 const readline = require('readline').createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
-readline.question('Article: ', number => {
-	url =  'https://www.constitutionofindia.net/constitution_of_india/23/articles/Article ' + number
+readline.question('Article: ', articleNumber => {
+	const url =  'https://www.constitutionofindia.net/constitution_of_india/23/articles/Article ' + articleNumber;
 	readline.close();
-	scrapeData()
+	scrapeData(url, articleNumber)
 });
 
-const scrapeData = async () => {
+const scrapeData = async (url, articleNumber) => {
 	try {
 		console.log('Fetching data...')
 		const { data } = await axios.get(url)
@@ -30,15 +28,16 @@ const scrapeData = async () => {
 			if(clause != '') {
 				// Add separator before new clause
 				if (id > 0 && !!clause.match(/^\(\d+\)/)) {
-					clauseArray.push('------')
+					clauseArray.push("\x1b[93m" + "------" + "\x1b[0m")
 				}
 				clauseArray.push(clause)
 			}
 		})
 
 		console.clear()
+		console.log("\x1b[1;93m" + `Article ${articleNumber}` + "\x1b[0m") //yellow bold
 		console.log(clauseArray.join('\n'))
 	} catch (err) {
-		console.log('Error: Request failed with status code '+err.response.status)
+		console.log('Error: Request failed with status code ' + err.response.status)
 	}
 }
